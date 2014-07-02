cb2ae5643a127586870c46d02e8b09ff504cedd8
primus1989 <primus1989@i.ua>
2014-07-02 22:33:20 +0300

    remove logo animation

--- js/logo.js
+++ js/logo.js
@@ -2,11 +2,13 @@ function diplay_hide (blockId)
 { 
     if ($(blockId).css('display') == 'none') 
         { 
-           $(blockId).animate({height  : 'show'}, 3000); 
+          /* $(blockId).animate({height  : 'show'}, 3000); 
            $(blockId).animate({color  : '#991613'}, 3500);
-            $(blockId).animate({marginTop  : '0px'}, 2000, function() {
-            	$('#logo-panel').fadeIn('slow');
-            //	$('#logo-panel').show();
-            });
+            $(blockId).animate({marginTop  : '0px'}, 2, function() {
+            	//$('#logo-panel').fadeIn('slow');
+            	$('#logo-panel').show();
+            });*/
+             
+            $('#logo-panel').show();
         } 
 } 
