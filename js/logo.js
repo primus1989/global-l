70969e5c4e48de94bb05b92dd28ff96ef37f1687
primus1989 <primus1989@i.ua>
2014-07-03 20:33:20 +0300

    add popup for create portfolio

--- js/logo.js
+++ js/logo.js
@@ -1,14 +1,13 @@
 function diplay_hide (blockId)
+
 { 
-    if ($(blockId).css('display') == 'none') 
+    /*if ($(blockId).css('display') == 'none') 
         { 
-          /* $(blockId).animate({height  : 'show'}, 3000); 
-           $(blockId).animate({color  : '#991613'}, 3500);
-            $(blockId).animate({marginTop  : '0px'}, 2, function() {
-            	//$('#logo-panel').fadeIn('slow');
-            	$('#logo-panel').show();
-            });*/
-             
-            $('#logo-panel').show();
-        } 
+            $(blockId).animate({height  : 'show'}, 3000); 
+            $(blockId).animate({color  : '#991613'}, 3500);
+            $(blockId).animate({marginTop  : '0px'}, 2000, function() {
+            	$('#logo-panel').fadeIn('slow');
+            });
+        } */
+        $('#logo-panel').show();
 } 
